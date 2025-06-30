7bd49362b5538e5744caea7bdb4a7b1daf8e1a0d
Bernitafebri <bernitafebri@gmail.com>
2025-06-30 13:36:34 +0700

    create api login with jwt and update readme

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get("/test-me", function () {
     return 'Hello from Laravel!';
 });
+
+Route::post('/login', LoginController::class)->name('login');
