48e0692e0b837f15837ec57551bc47928300706b
Bernitafebri <bernitafebrihatin@students.undip.ac.id>
2025-07-04 22:16:12 +0700

    update template admin, coba login layouts sendiri

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,29 @@
-import "./bootstrap";
+// import "./bootstrap";
+// import { createApp } from "vue";
+// // import "../css/app.css";
+// import router from "./router.js";
+
+// import App from "./App.vue";
+
+// // createApp(App).mount("#app");
+// createApp(App).use(router).mount("#app");
+
+// import "./assets/main.css";
+// // Import Swiper styles
+// import "swiper/css";
+// import "swiper/css/navigation";
+// import "swiper/css/pagination";
+// import "jsvectormap/dist/jsvectormap.css";
+// import "flatpickr/dist/flatpickr.css";
+
 import { createApp } from "vue";
-// import "../css/app.css";
-import router from "./router.js";
-
 import App from "./App.vue";
+import router from "./router";
+// import VueApexCharts from "vue3-apexcharts";
 
-// createApp(App).mount("#app");
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+
+app.use(router);
+// app.use(VueApexCharts);
+
+app.mount("#app");
